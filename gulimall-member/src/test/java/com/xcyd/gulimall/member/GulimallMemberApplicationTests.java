b75cba6c80090121b62d010b6e9a483417c2dd07
lixiaohui <2947579360@qq.com>
2023-07-24 15:22:07 +0800

    项目初始结构创建

---
+++ gulimall-member/src/test/java/com/xcyd/gulimall/member/GulimallMemberApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xcyd.gulimall.member;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GulimallMemberApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
